73805e4c37b245e8bde63afe2b7152f12db8c9ae
hyerichung <hyerichungonearth@gmail.com>
2021-08-02 23:45:46 +0900

    [ADD] apply store in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
