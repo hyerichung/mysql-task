94cd82554026def84796bb66516342a207f9431f
hyerichung <hyerichungonearth@gmail.com>
2021-08-04 05:26:52 +0900

    [EDIT] config index.js

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -5,12 +5,6 @@ env.NODE_ENV = env.NODE_ENV || "development";
 
 module.exports = {
   port: normalizePort(env.PORT || "5000"),
-  s3: {
-    bucketName: process.env.AWS_BUCKET_NAME,
-    region: process.env.AWS_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 };
 
 /**
